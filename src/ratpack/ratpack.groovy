25b50428db185acb4f2a02d22ee32ca1c11c6798
R. Tyler Croy <tyler@monkeypox.org>
2014-11-23 10:11:42 -0800

    Add a simple build.gradle and ratpack handler to get us started with

---
+++ src/ratpack/ratpack.groovy
@@ -0,0 +1,34 @@
+import ratpack.handlebars.HandlebarsModule
+import ratpack.jackson.JacksonModule
+
+import static ratpack.handlebars.Template.handlebarsTemplate
+import static ratpack.jackson.Jackson.json
+import static ratpack.groovy.Groovy.*
+import static ratpack.websocket.WebSockets.websocket
+
+ratpack {
+    bindings {
+        add new HandlebarsModule()
+        add new JacksonModule()
+    }
+    handlers {
+        get {
+            render 'offtopic!'
+        }
+
+        /* set up a demo/dummy websocket listener */
+        get("ws") { context ->
+            websocket(context) { ws ->
+            } connect {
+                it.onClose {
+                    println "closing"
+                } onMessage {
+                    "client sent me ${it}"
+                }
+            }
+        }
+
+        assets 'public'
+    }
+}
+
